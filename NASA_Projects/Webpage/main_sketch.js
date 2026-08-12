166883baf39fd576b01eb52278c0a680a4c37091
MaroRose <64484597+MaroRose@users.noreply.github.com>
2026-08-12 05:07:24 -0500

    Adding missing files
    jwst_james_webb_space_telescope.obj
    main_sketch.js

---
+++ NASA_Projects/Webpage/main_sketch.js
@@ -0,0 +1,35 @@
+let myModel;
+
+function preload() {
+  // Load the OBJ file from the models folder
+  myModel = loadModel("jwst_james_webb_space_telescope.obj", true);
+}
+
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
+
+  // Smooth rendering
+  smooth();
+}
+
+// function draw() {
+//   background(30);
+
+
+//   // Lighting
+//   ambientLight(100);
+//   directionalLight(255, 255, 255, 0, 0, -1);
+
+//   //flip upside down
+//   rotateZ(PI);
+
+//   // Mouse-controlled camera
+//   orbitControl();
+
+//   // Adjust the size of the model
+//   scale(2);
+
+//   // Display the model
+//   model(myModel);
+// }
+
